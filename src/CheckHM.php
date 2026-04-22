573d9e082095328d4a34b014c6e0cc256c14ad67
bdx000 <nickok@qq.com>
2026-04-22 17:15:38 +0800

    生成8位随机字符串

--- src/CheckHM.php
+++ src/CheckHM.php
@@ -17,4 +17,33 @@ class CheckHM{
         }
         return preg_match('/^[a-fA-F0-9]{2}(\.[a-fA-F0-9]{2}){7}$/', $dog);
     }
+
+    // 生成随机字符串
+    static public function GetSequeid(){ 
+        $t1='1970-01-01';
+		$t2= date("Y-m-d H:i:s");
+		$arr[]=rand(1,9);
+		$randVal = strval($arr[0]);
+		$date1 = strtotime($t1);
+		$date2 = strtotime($t2);
+		$diff = $date2-$date1;
+		$diff1 = substr($diff,0,strlen($diff) - 2);
+		$diff2 = substr($diff,strlen($diff) - 1,1);
+		$strDiff = $diff1.$randVal.$diff2;
+		$strDiffDecHex = strval(dechex($strDiff));
+		$len = strlen($strDiffDecHex);
+		$beginPos = 0;
+		if($len > 8){
+		    $beginPos = $len - 8;
+		    $strDiffDecHex = substr($strDiff, $beginPos);
+		    $strDiff = $strDiffDecHex;
+		}else{
+		    $beginPos = 8 - $len;
+		    $strDiff = $strDiffDecHex;
+		    for($i = 0;$i<= $beginPos - 1;$i++){
+		        $strDiff = "0".$strDiff;
+		    }
+		}
+		return $strDiff;
+    }
 }
