18814accced6ba9283918f952b0049beb657a9b3
bdx000 <nickok@qq.com>
2026-04-27 17:45:15 +0800

    修改mac验证逻辑

--- src/CheckHM.php
+++ src/CheckHM.php
@@ -2,20 +2,14 @@
 namespace Caidaoke;
 class CheckHM{ 
     //  检查MAC地址:00.00.00.00.00.00
-    static public function mac(string $mac, string $separator='.'){
-        if($separator === '.'){
-            return preg_match('/^[a-fA-F0-9]{2}(\.[a-fA-F0-9]{2}){5}$/', $mac);
-        } else if($separator === ':'){
-            return preg_match('/^[a-fA-F0-9]{2}(:[a-fA-F0-9]{2}){5}$/', $mac);
-        }
-        return preg_match('/^[a-fA-F0-9]{12}$/', $mac);
+    static public function mac(string $mac){
+        return preg_match('/^[a-fA-F0-9]{12}$/', $mac) || 
+		preg_match('/^[a-fA-F0-9]{2}(:[a-fA-F0-9]{2}){5}$/', $mac) ||
+		preg_match('/^[a-fA-F0-9]{2}(\.[a-fA-F0-9]{2}){5}$/', $mac);
     }
     // 检查加密狗地址:00.00.00.00.00.00.00.00
-    static public function dog(string $dog, string $separator='.'){
-        if(empty($separator)){
-            return preg_match('/^[a-fA-F0-9]{16}$/', $dog);
-        }
-        return preg_match('/^[a-fA-F0-9]{2}(\.[a-fA-F0-9]{2}){7}$/', $dog);
+    static public function dog(string $dog){
+        return preg_match('/^[a-fA-F0-9]{2}(\.[a-fA-F0-9]{2}){7}$/', $dog) || preg_match('/^[a-fA-F0-9]{16}$/', $dog);
     }
 
     // 生成随机字符串
